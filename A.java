d04500221f01a4437d1e72ab36d2f7a18b4259e2
gitsukumar <35107890+gitsukumar@users.noreply.github.com>
2018-01-04 15:39:18 +0000

    Create A.java

---
+++ A.java
@@ -0,0 +1,15 @@
+class A
+{
+
+public static void main(String a[])
+
+{
+
+System.out.println("..............********..........");
+System.out.println("..............****sukumar****..........");
+System.out.println("..............*****Hello World***..........");
+
+System.out.println("..............********..........");
+
+
+}}
